ede584b17e1ef6e0a9bafeb0ed402da694ebb5ca
Emirhan Akpınar <leetpocketmine@gmail.com>
2022-05-06 22:48:44 +0300

    Fixed eslint rules warnings of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -67,7 +67,7 @@ const Home = () => {
         <div className="h-full">
             <nav className="h-20 flex items-center justify-between px-64 border-b border-b-neutralGrayLightest">
                 <div className="flex gap-x-12">
-                    <img src={logo}/>
+                    <img src={logo} alt="Logo"/>
 
                     <div className="relative">
                         <input className="h-10 text-neutralBlack pl-3 pr-10 w-56 bg-neutralGrayLightest rounded-lg focus:outline-none focus:ring-0 focus:shadow transition-shadow placeholder:text-sm placeholder-neutralGray" placeholder="Search"/>
@@ -80,21 +80,21 @@ const Home = () => {
 
                 <div className="flex gap-x-5 items-center">
                     <div className="flex gap-x-7 pr-5 py-1 border-r border-r-neutralGrayLightest">
-                        <a className="text-neutralBlack font-semibold hover:text-neutralGrayDark transition-colors" href="#">Restaurants</a>
-                        <a className="text-neutralBlack font-semibold hover:text-neutralGrayDark transition-colors" href="#">Deals</a>
+                        <a href="/" className="text-neutralBlack font-semibold hover:text-neutralGrayDark transition-colors">Restaurants</a>
+                        <a href="/" className="text-neutralBlack font-semibold hover:text-neutralGrayDark transition-colors">Deals</a>
                     </div>
 
                     <div className="flex items-center">
-                        <a className="text-neutralBlack font-semibold hover:text-neutralGrayDark transition-colors mr-7" href="#">My orders</a>
+                        <a href="/" className="text-neutralBlack font-semibold hover:text-neutralGrayDark transition-colors mr-7">My orders</a>
 
-                        <a href="#" className="h-12 w-12 relative flex items-center justify-center bg-primaryLight rounded-2xl hover:bg-neutralGrayLightest transition-colors mr-4">
+                        <a href="/" className="h-12 w-12 relative flex items-center justify-center bg-primaryLight rounded-2xl hover:bg-neutralGrayLightest transition-colors mr-4">
                             <BasketIcon/>
 
                             <span className="absolute -inset-y-2 right-0 text-white h-5 w-5 flex items-center justify-center text-xs font-semibold bg-primary rounded-lg">4</span>
                         </a>
 
-                        <a href="#" className="h-12 w-12 flex p-px items-center justify-center border-2 border-neutralGrayLightest rounded-2xl hover:opacity-90 transition-opacity">
-                            <img src={avatar}/>
+                        <a href="/" className="h-12 w-12 flex p-px items-center justify-center border-2 border-neutralGrayLightest rounded-2xl hover:opacity-90 transition-opacity">
+                            <img src={avatar} alt="Avatar"/>
                         </a>
                     </div>
                 </div>
@@ -104,7 +104,7 @@ const Home = () => {
                 <header className="h-64 py-5">
                     <div className="h-full flex items-center gap-x-8">
                         <div className="h-full flex w-1/2 group overflow-hidden gap-x-4 pr-8 bg-primaryLight rounded-2xl cursor-pointer hover:shadow transition-shadow">
-                            <img className="group-hover:scale-105 transition-transform" src={dessert}/>
+                            <img className="group-hover:scale-105 transition-transform" src={dessert} alt="All deserts"/>
 
                             <div className="flex flex-col pt-6 pb-4">
                                 <h5 className="text-xl font-bold text-neutralBlack mb-3">All deserts</h5>
@@ -116,7 +116,7 @@ const Home = () => {
                         </div>
 
                         <div className="h-full flex-1 group overflow-hidden flex gap-x-4 pr-8 bg-secondaryLight rounded-2xl cursor-pointer hover:shadow transition-shadow">
-                            <img className="group-hover:scale-105 transition-transform" src={hamburger}/>
+                            <img className="group-hover:scale-105 transition-transform" src={hamburger} alt="Big Burgers"/>
 
                             <div className="flex flex-col pt-6 pb-4">
                                 <h5 className="text-xl font-bold text-neutralBlack mb-3">Big Burgers</h5>
